57cd4e8dcfa77452f4f73ddb1cf8dfd37dc118c0
NazarYavor26 <92759124+NazarYavor26@users.noreply.github.com>
2022-11-19 21:40:36 +0200

    added SparePartProduct and enum SparePartDeviceModel

---
+++ Cactus-API/Cactus-API.DAL/Entities/SparePartProduct.cs
@@ -0,0 +1,24 @@
+﻿using Cactus_API.DAL.Enums;
+using System.ComponentModel.DataAnnotations;
+
+namespace Cactus_API.DAL.Entities
+{
+    public class SparePartProduct
+    {
+        public int Id { get; set; }
+
+        [Required]
+        [MaxLength(100)]
+        public string Title { get; set; }
+
+        [Required]
+        public double Price { get; set; }
+
+        public SparePartDeviceModel? DeviceModel { get; set; }
+
+        [Required]
+        public string Category { get; set; }
+
+
+    }
+}
---
+++ Cactus-API/Cactus-API.DAL/Enums/SparePartDeviceModel.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Cactus_API.DAL.Enums
+{
+    public enum SparePartDeviceModel
+    {
+        Components = 1,
+        Tires,
+        InnerTubes,
+        OtherSmallParts,
+        Fenders,
+        Covers
+    }
+}
